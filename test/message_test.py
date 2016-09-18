b98ebdb592317d03d0064bb1db66240f302b2f68
peitur <peitur@gmail.com>
2016-09-18 23:54:21 +0200

    WIP, started testcase for messages

--- test/message_test.py
+++ test/message_test.py
@@ -9,30 +9,37 @@ import unittest
 class MessageTest( unittest.TestCase ):
 
     def setUp( self ):
-        self.message_list = []
+        self.ok_message_list = {
+            'RequestMessage': [{"to": "aaaa", "content": "cccccccc", "id": "1", "from": "bbbb", "type": "request"}],
+            'ReplyMessage': [{"to": "aaaa", "content": "cccccccc", "id": "2", "from": "bbbb", "type": "reply"}],
+            'InfoMessage': [{"to": "aaaa", "content": "cccccccc", "id": "3", "from": "bbbb", "type": "info"}],
+            'UpdateMessage': [{"to": "aaaa", "content": "cccccccc", "id": "4", "from": "bbbb", "type": "update"}],
+            'AlarmMessage': [{"to": "aaaa", "content": "cccccccc", "id": "5", "from": "bbbb", "type": "alarm"}]
+        }
+
+        self.nok_message_list = {
+            'RequestMessage': [{"to": "aaaa", "content": "cccccccc", "id": "1", "from": "bbbb", "type": "test"}]
+        }
 
 
     def tearDown( self ):
         pass
 
     def test_decode( self ):
-        pass
+
+        for o in self.ok_message_list:
+            for k in self.ok_message_list[ o ]:
+                self.assertEqual( type( decode_message( k ) ).__name__, o )
+
+        for o in self.nok_message_list:
+            for k in self.nok_message_list[ o ]:
+                self.assertRaises( AttributeError, decode_message, k )
 
     def test_encode( self ):
         pass
 
     def test_alarm_message( self ):
-
-        ok_message = [
-            { 'mtype' : 'alarm', 'mid' : '1', 'mto' : 'aaaaa', 'mfrom' : 'bbbb', 'data' : {'message':'hello', 'state':'ok' } }
-        ]
-
-        nok_message = [
-            { 'mtype' : 'test', 'mid' : 'abc', 'mto' : 'aaaaa', 'mfrom' : 'bbbb', 'data' : {'message':'hello', 'state':'ok' } }
-        ]
-
-        for ok in ok_message:
-            self.assertEqual( type( AlarmMessage( ok['mid'], ok['mto'], ok['mfrom'], ok['data'] ) ).__name__, 'AlarmMessage' )
+        pass
 
 
     def test_request_message( self ):
